220674fc338380a1821291d6a13834b8a461061b
Jaakko Heusala <jhh@hg.fi>
2023-12-31 03:54:20 +0200

    Refactored to correct hierarchy

--- ecb/EcbBackendUtils.ts
+++ ecb/EcbBackendUtils.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-import { createCurrencyRates, CurrencyRates } from "../../../core/types/CurrencyRates";
+import { createCurrencyRates, CurrencyRates } from "../../core/types/CurrencyRates";
 import { fetch } from "ecb-euro-exchange-rates";
 
 export class EcbBackendUtils {
